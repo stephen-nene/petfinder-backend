2cadfcd1eaad254138a1b527d81a2f7594376862
+stephen.nene <+>
2023-03-06 17:34:43 +0300

    all domains

--- app/controller/pets_controller.rb
+++ app/controller/pets_controller.rb
@@ -10,7 +10,7 @@ class PetsController < Sinatra::Base
   private
 
   def set_cors_headers
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:3001'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
   end
