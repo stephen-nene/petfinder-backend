eebdbf6ca37fd8bfe5e217d77c7040915c7021cf
+stephen.nene <+>
2023-03-06 17:02:24 +0300

    added actual link

--- app/controller/pets_controller.rb
+++ app/controller/pets_controller.rb
@@ -10,7 +10,7 @@ class PetsController < Sinatra::Base
   private
 
   def set_cors_headers
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:9292'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
   end
