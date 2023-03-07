773823672bc0aef304b80d93961d95480771485e
+stephen.nene <+>
2023-03-07 08:33:30 +0300

    addeda *

--- app/controller/user_controller.rb
+++ app/controller/user_controller.rb
@@ -8,7 +8,7 @@ class UserController < Sinatra::Base
   private
 
   def set_cors_headers
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
 
